e0583bb473933df3dd6219d4733e5d3205b4ee89
Edgar Herrera <edgarh09lfc@gmail.com>
2023-08-30 03:28:55 -0700

    Add more stocks

--- index.js
+++ index.js
@@ -16,9 +16,39 @@ const stocks = {
         'dividend': 18.40,
         'pay': 'quarterly',
     },
+    'BA': {
+        'name': 'Boeing Company',
+        'dividend': 8.22,
+        'pay': 'quarterly',
+    },
+    'C': {
+        'name': 'Citigroup, Inc.',
+        'dividend': 2.12,
+        'pay': 'quarterly',
+    },
+    'CAT': {
+        'name': 'Caterpillar, Inc.',
+        'dividend': 5.20,
+        'pay': 'quarterly',
+    },
     'CSCO': {
         'name': 'Cisco Systems',
         'dividend': 1.56,
+        'pay': 'quarterly',
+    },
+    'D': {
+        'name': 'Dominion Energy, Inc',
+        'dividend': 2.67,
+        'pay': 'quarterly',
+    },
+    'F': {
+        'name': 'Ford Motor Company',
+        'dividend': 0.60,
+        'pay': 'quarterly',
+    },
+    'JNJ': {
+        'name': 'Johnson & Johnson',
+        'dividend': 4.76,
         'pay': 'quarterly',
     },
     'KHC': {
@@ -26,11 +56,36 @@ const stocks = {
         'dividend': 1.60,
         'pay': 'quarterly',
     },
+    'O': {
+        'name': 'Realty Income Corporation',
+        'dividend': 3.066,
+        'pay': 'Monthly',
+    },
+    'PEP': {
+        'name': 'PepsiCo',
+        'dividend': 5.06,
+        'pay': 'quarterly',
+    },
     'SBUX': {
         'name': 'Starbucks Corporation',
         'dividend': 2.12,
         'pay': 'quarterly',
     },
+    'SPG': {
+        'name': 'Simon Property Group',
+        'dividend': 7.40,
+        'pay': 'quarterly',
+    },
+    'TGT': {
+        'name': 'Target Corporation',
+        'dividend': 4.40,
+        'pay': 'quarterly',
+    },
+    'WMT': {
+        'name': 'Walmart Inc.',
+        'dividend': 2.28,
+        'pay': 'quarterly',
+    }
 }
 
 app.get('/api/:stockName', (req, res) => {
